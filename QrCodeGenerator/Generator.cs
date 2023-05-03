23134d4d7eaa5412c9da3e66eb8a3cd6b87abcde
Justin <ohmlotusphoenix@gmail.com>
2023-05-03 05:31:11 -0500

    merge conflict push

--- QrCodeGenerator/Generator.cs
+++ QrCodeGenerator/Generator.cs
@@ -1,29 +1,21 @@
-<<<<<<< Updated upstream
-﻿using System;
-=======
-﻿
+using System;
 using MySqlConnector;
 using Org.BouncyCastle.Utilities;
-using System;
->>>>>>> Stashed changes
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Net.NetworkInformation;
 using ZXing;
 using ZXing.QrCode;
+using MySql.Data.MySqlClient;
 
 namespace QrCodeGenerator
 {
     public class Generator
     {
-<<<<<<< Updated upstream
-        public static void GenerateQRCode(string content, string filePath)
-=======
         /* This code defines a class called Generator in the namespace QrCodeGenerator. The class contains a single static method called GenerateQRCode, which takes in three parameters: the content for the QR code, the filename to save the QR code as, and a connection string to the MySQL database. */
         /* The GenerateQRCode method in the Generator class, which generates and saves the QR code image to the database. This method could be extracted into a separate class or controller method. */
         public static void GenerateQRCode(string content, string fileName, string connectionString)
->>>>>>> Stashed changes
         {
             /*These lines create a QR code using the ZXing library. The BarcodeWriterPixelData object generates a pixel data representation of the QR code with specified dimensions and options. The resulting pixel data is stored in the pixelData variable.  */
             var qrWriter = new BarcodeWriterPixelData
@@ -37,7 +29,7 @@ namespace QrCodeGenerator
                 }
             };
             /*These lines create a bitmap image from the pixel data, convert it to a PNG format and save it to a memory stream.The image bytes are then stored in the imageBytes variable.*/
-                        var pixelData = qrWriter.Write(content);
+            var pixelData = qrWriter.Write(content);
             using var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb);
             var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
             try
@@ -49,17 +41,12 @@ namespace QrCodeGenerator
                 bitmap.UnlockBits(bitmapData);
             }
 
-<<<<<<< Updated upstream
-            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            bitmap.Save($@"{desktopPath}\{filePath}", ImageFormat.Png);
-=======
             using var ms = new MemoryStream();
             bitmap.Save(ms, ImageFormat.Png);
             var imageBytes = ms.ToArray();
 
-
             /*These lines connect to the MySQL database using the connection string provided as an argument. Then, a SQL statement is created to insert the filename and image bytes into the qrcodes table. The command.Parameters.AddWithValue method is used to set the values for the parameters in the SQL statement, and the command.ExecuteNonQuery method is used to execute the SQL statement.  */
-            using var connection = new MySqlConnection($"server=localhost;database=qrcodes;user=qrcodes;password=qrcodes;");
+            using var connection = new MySqlConnection(connectionString);
             connection.Open();
             //the code (sql statement) below will be changed to take in the inventory data
             using var command = new MySqlCommand("INSERT INTO qrcodes (filename, content) VALUES (@fileName, @content)", connection);
@@ -67,7 +54,6 @@ namespace QrCodeGenerator
             command.Parameters.AddWithValue("@fileName", fileName);
             command.Parameters.AddWithValue("@content", imageBytes);
             command.ExecuteNonQuery();
->>>>>>> Stashed changes
         }
     }
 }
